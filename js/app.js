26ccd8d7c34d6a686bc2b24e74c49ea45e5866bf
Laringitis <cabrera8lara@gmail.com>
2023-01-29 00:51:25 -0300

    ejercicio 1

---
+++ js/app.js
@@ -0,0 +1,11 @@
+/* let nombre = prompt ("¿Como te llamas?");
+const bienvenida= "Buenas noches";
+const kiut= ",sos muy kiut";
+const espacio= " "
+const frase= bienvenida+espacio+nombre+espacio+kiut;
+console.log (frase)
+ */
+let numeroA = parseInt(prompt("Inserte numero"))
+let numeroB = parseInt(prompt("Inserte otro numero"))
+const calculadora = (numeroA+numeroB) 
+alert (calculadora)
