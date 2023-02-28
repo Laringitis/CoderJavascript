dea9816549edf196f4b3f680574ccc92cbc074e3
Laringitis <cabrera8lara@gmail.com>
2023-02-28 16:00:19 -0300

    html masomenos

--- js/app.js
+++ js/app.js
@@ -5,7 +5,16 @@ const espacio= " "
 const frase= bienvenida+espacio+nombre+espacio+kiut;
 console.log (frase)
  */
-let numeroA = parseInt(prompt("Inserte numero"))
-let numeroB = parseInt(prompt("Inserte otro numero"))
-const calculadora = (numeroA+numeroB) 
-alert (calculadora)
+// let numeroA = parseInt(prompt("Inserte numero"))
+// let numeroB = parseInt(prompt("Inserte otro numero"))
+// const calculadora = (numeroA+numeroB) 
+// alert (calculadora)
+
+// let numero = Number(prompt ("ingrese un numero"))
+//     if (numero >=10 && numero<=50){
+//     alert("su numero esta entre 10 y 50")
+//     }else{
+//         alert("su numero no esta entre el 10 y 50")
+//     }
+
+let
