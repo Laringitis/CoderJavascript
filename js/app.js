d4750feea92af31f4bdd69649ef712c2e432271b
Laringitis <cabrera8lara@gmail.com>
2023-03-01 01:01:31 -0300

    esqueleto echo faltan algunas verficiaciones

--- js/app.js
+++ js/app.js
@@ -1,20 +1,49 @@
-/* let nombre = prompt ("¿Como te llamas?");
-const bienvenida= "Buenas noches";
-const kiut= ",sos muy kiut";
-const espacio= " "
-const frase= bienvenida+espacio+nombre+espacio+kiut;
-console.log (frase)
- */
-// let numeroA = parseInt(prompt("Inserte numero"))
-// let numeroB = parseInt(prompt("Inserte otro numero"))
-// const calculadora = (numeroA+numeroB) 
-// alert (calculadora)
+const correctoPlantas= ["rosa", "clavel", "margarita"]
+class Planta{
+    constructor(nombre, precio, stock) {
+    this.nombre= nombre;
+    this.precio= parseFloat(precio);
+    this.stock= parseFloat(stock);
+    }
+    suficienteStock(cantidad){
+        if( this.stock >= cantidad ){
+            this.stock = this.stock - cantidad
+            return true
+        }
+        else{
+            return false
+        }
+    }
+}
 
-// let numero = Number(prompt ("ingrese un numero"))
-//     if (numero >=10 && numero<=50){
-//     alert("su numero esta entre 10 y 50")
-//     }else{
-//         alert("su numero no esta entre el 10 y 50")
+listaPlantas=[];
+listaPlantas.push(rosa =  new Planta ("rosa", 150, 24));
+listaPlantas.push(clavel = new Planta ("clavel", 300, 42));
+listaPlantas.push(margarita = new Planta ("margarita", 200, 32));
+
+
+function disponibleCompra(Planta){
+    return Planta.nombre==comprar
+}
+
+let comprar = prompt("¿Que desea comprar? teclee 'fin' cuando desee terminar la compra");
+let buscarDisponible= listaPlantas.find(disponibleCompra)
+    if(disponibleCompra=false){
+        alert ("No contamos con esa planta disponible.")
+    }
+let cantidad= prompt ("¿Cuanto desea comprar? teclee 'fin' si desea volver a empezar")
+
+
+if (buscarDisponible.suficienteStock(cantidad)){
+    alert ("Gracias por comprar")
+}else {
+    alert ("no contamos con stock")
+}
+
+// if (comprar==plantas){
+//     if (cantidad<= rosa.stock){
+//         let comprado = (rosa.precio * cantidad);
 //     }
-
-let
+// }else {
+//     alert ("su pedido no existe")
+// }
