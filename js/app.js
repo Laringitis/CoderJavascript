68ad0d054d2254eb7662c7f274a900d67b725116
Laringitis <cabrera8lara@gmail.com>
2023-03-01 11:59:30 -0300

    no funciona el total :(

--- js/app.js
+++ js/app.js
@@ -4,7 +4,12 @@ class Planta{
     this.nombre= nombre;
     this.precio= parseFloat(precio);
     this.stock= parseFloat(stock);
+
     }
+    total(){
+        return (this.precio * cantidad)
+    }
+    
     suficienteStock(cantidad){
         if( this.stock >= cantidad ){
             this.stock = this.stock - cantidad
@@ -21,6 +26,9 @@ listaPlantas.push(rosa =  new Planta ("rosa", 150, 24));
 listaPlantas.push(clavel = new Planta ("clavel", 300, 42));
 listaPlantas.push(margarita = new Planta ("margarita", 200, 32));
 
+function recargar () {
+    window.location.href = window.location.href;
+}
 
 function disponibleCompra(Planta){
     return Planta.nombre==comprar
@@ -28,18 +36,23 @@ function disponibleCompra(Planta){
 
 let comprar = prompt("¿Que desea comprar? teclee 'fin' cuando desee terminar la compra");
 let buscarDisponible= listaPlantas.find(disponibleCompra)
-    if(disponibleCompra=false){
+    if(disponibleCompra = undefined){
         alert ("No contamos con esa planta disponible.")
+        location.reload
+
     }
 let cantidad= prompt ("¿Cuanto desea comprar? teclee 'fin' si desea volver a empezar")
-
 
 if (buscarDisponible.suficienteStock(cantidad)){
     alert ("Gracias por comprar")
 }else {
     alert ("no contamos con stock")
-}
+    recargar()
+}    
 
+
+
+document.write ("usted comrpro " + cantidad + "del producto " + comprar + "<br> El total a pagar es " + total(comprar))
 // if (comprar==plantas){
 //     if (cantidad<= rosa.stock){
 //         let comprado = (rosa.precio * cantidad);
